c691380b9b018f505f1651b08d2125a7c97f1d08
edelwud <edelwud@icloud.com>
2021-09-09 18:28:04 +0300

    update: sequence generation

--- random/libs/random/include/random/random.h
+++ random/libs/random/include/random/random.h
@@ -1,11 +1,8 @@
 #pragma once
 
+#include <vector>
+
 class Random {
 public:
-  static unsigned int Generate(int from, int to);
-  static void SetSeed(int seed);
-
-private:
-  static inline int primeNumber = 1103515245;
-  static inline int currentSeed = 1;
+    static std::vector<double> GenerateSequence(int a, int m, int r0, int length);
 };
--- random/libs/random/src/random.cpp
+++ random/libs/random/src/random.cpp
@@ -1,8 +1,11 @@
 #include <random.h>
 
-unsigned int Random::Generate(int from, int to) {
-  currentSeed = currentSeed * primeNumber + 12345;
-  return static_cast<unsigned int>(currentSeed * 65536) % (to - from) + from;
+std::vector<double> Random::GenerateSequence(int a, int m, int r0, int length) {
+    std::vector<double> sequence(length);
+    std::generate(sequence.begin(), sequence.end(), [a, m, r0]() {
+        static int rn = r0;
+        rn = rn * a % m;
+        return (double)rn / (double)m;
+    });
+    return sequence;
 }
-
-void Random::SetSeed(int seed) { currentSeed = seed; }
