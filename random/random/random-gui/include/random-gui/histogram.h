46bfe70436edd844fc02a713ad2ae09382acabe7
edelwud <edelwud@icloud.com>
2021-09-10 00:22:58 +0300

    add: interacting actions

--- random/random/random-gui/include/random-gui/histogram.h
+++ random/random/random-gui/include/random-gui/histogram.h
@@ -3,27 +3,30 @@
 #include <Mahi/Gui.hpp>
 #include <Mahi/Util.hpp>
 
-#define WINDOW_WIDTH 700
-#define WINDOW_HEIGHT 400
+#define WINDOW_WIDTH 1920
+#define WINDOW_HEIGHT 1080
 #define WINDOW_TITLE "Visual interface for Random library"
 
 using namespace mahi::gui;
 using namespace mahi::util;
 
 class RandomHistogram : public Application {
-public:
-  RandomHistogram();
+  public:
+    RandomHistogram();
 
-public:
-  void update() override;
-  void SetRandomParameters(int a, int m, int r0, int sequenceLength,
-                           int positionLength, int from, int to);
+  public:
+    void update() override;
+    void SetRandomParameters(int a, int m, int r0, int sequenceLength,
+                             int positionLength, int from, int to);
 
-private:
-  std::vector<int> positions;
-  std::vector<int> values;
-  std::vector<double> sequence;
+  private:
+    std::vector<int> positions;
+    std::vector<int> values;
+    std::vector<double> sequence;
 
-  int maxY;
-  int length;
+    int maxY;
+    int length;
+
+  private:
+    int a = 126398, m = 911201, r0 = 1, sequenceLength = 1000000;
 };
--- random/random/random-gui/src/histogram.cpp
+++ random/random/random-gui/src/histogram.cpp
@@ -6,37 +6,43 @@
 
 RandomHistogram::RandomHistogram()
     : Application(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE) {
-  set_vsync(false);
-  ImGui::DisableViewports();
+    set_vsync(false);
+    ImGui::DisableViewports();
 }
 
 void RandomHistogram::update() {
-  std::string subject;
-  auto [w, h] = get_window_size();
-  ImGui::SetNextWindowPos({0, 0}, ImGuiCond_Always);
-  ImGui::SetNextWindowSize(ImVec2(w, h), ImGuiCond_Always);
+    ImGui::BeginFixed("Random parameters", {0, 0},
+                      {WINDOW_WIDTH / 4, WINDOW_HEIGHT});
+    ImGui::Text("1000 pts ea. @ %.3f FPS", ImGui::GetIO().Framerate);
+    ImGui::InputInt("A", &a);
+    ImGui::InputInt("M", &m);
+    ImGui::InputInt("r0", &r0);
+    ImGui::InputInt("length", &sequenceLength);
+    if (ImGui::Button("Apply"))
+        SetRandomParameters(a, m, r0, sequenceLength, 20, 0, 1);
+    ImGui::End();
 
-  ImGui::Begin(WINDOW_TITLE, nullptr, ImGuiWindowFlags_NoTitleBar);
-  ImGui::Text("1000 pts ea. @ %.3f FPS", ImGui::GetIO().Framerate);
+    ImGui::BeginFixed(WINDOW_TITLE, {WINDOW_WIDTH / 4, 0},
+                      {WINDOW_WIDTH * 3 / 4, WINDOW_HEIGHT - 100});
 
-  ImGui::InputText("Subject     ", &subject);
-
-  ImPlot::SetNextPlotLimits(-1, length, 0, maxY);
-  if (ImPlot::BeginPlot("##Histogram", nullptr, nullptr, ImVec2(-1, -1),
-                        ImPlotFlags_NoChild)) {
-    ImPlot::PlotBars("Bar", positions.data(), values.data(), length, 0.5f);
-    ImPlot::EndPlot();
-  }
-  ImGui::End();
+    ImPlot::SetNextPlotLimits(-1, length, 0, maxY);
+    if (ImPlot::BeginPlot("##Histogram", nullptr, nullptr, ImVec2(-1, -1),
+                          ImPlotFlags_NoChild)) {
+        ImPlot::PlotBars("Level", positions.data(), values.data(), length,
+                         0.5f);
+        ImPlot::EndPlot();
+    }
+    ImGui::End();
 }
 
-void RandomHistogram::SetRandomParameters(int a, int m, int r0,
-                                          int sequenceLength,
+void RandomHistogram::SetRandomParameters(int _a, int _m, int _r0,
+                                          int _sequenceLength,
                                           int positionLength, int from,
                                           int to) {
-  length = positionLength;
-  positions = Histogram::Utils::GenerateXPositions(length, 0, 1);
-  sequence = Random::GenerateSequence(a, m, r0, sequenceLength);
-  values = Histogram::Utils::GetXRandomValuesCount(sequence, length, from, to);
-  maxY = *std::max_element(values.begin(), values.end());
+    length = positionLength;
+    positions = Histogram::Utils::GenerateXPositions(length, 0, 1);
+    sequence = Random::GenerateSequence(_a, _m, _r0, _sequenceLength);
+    values =
+        Histogram::Utils::GetXRandomValuesCount(sequence, length, from, to);
+    maxY = *std::max_element(values.begin(), values.end());
 }
