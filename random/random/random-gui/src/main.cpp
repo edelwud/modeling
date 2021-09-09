4437b981aed6e9d05a74e82066fbd318c23ce884
edelwud <edelwud@icloud.com>
2021-09-09 16:29:03 +0300

    integrate: graph bars feature

--- random/random/random-gui/src/main.cpp
+++ random/random/random-gui/src/main.cpp
@@ -1,28 +1,10 @@
-#include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/screen.hpp>
-#include <vector>
+#define MAHI_GUI_NO_CONSOLE
+#define MAHI_GUI_USE_DISCRETE_GPU
 
-#include "ftxui/dom/node.hpp"
-#include "ftxui/screen/box.hpp"
-#include "ftxui/screen/color.hpp"
-
-#include <random/random.h>
+#include <graph.h>
 
 int main() {
-  using namespace ftxui;
-  Element document = graph([](int x, int y) {
-                       std::vector<int> result(x, 0);
-                       for (int i = 1; i < x; ++i) {
-                         result[i] = Random::Generate(i, x) / y;
-                       }
-                       return result;
-                     }) |
-                     color(Color::Red) | border | color(Color::White) |
-                     bgcolor(Color::Black);
-
-  auto screen = Screen::Create(Dimension::Fixed(80), Dimension::Fixed(10));
-  Render(screen, document);
-  screen.Print();
-
-  return EXIT_SUCCESS;
+  GraphBars demo;
+  demo.run();
+  return 0;
 }
