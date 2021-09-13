2165d53dba8b19be76520cc489beec074bdda03f
edelwud <edelwud@icloud.com>
2021-09-14 00:46:41 +0300

    add: uniform distribution rendering capability

--- distribution/distribution/distribution-gui/include/distribution-gui/uniform_histogram.h
+++ distribution/distribution/distribution-gui/include/distribution-gui/uniform_histogram.h
@@ -1,6 +1,26 @@
 #pragma once
 
+#include <vector>
+
 class UniformDistributionHistogram {
   protected:
     void RenderUniformHistogram();
+
+  private:
+    void calculate();
+
+  private:
+    std::vector<int> positions;
+    std::vector<int> values;
+    std::vector<double> sequence;
+
+  private:
+    int a = 0;
+    int b = 0;
+    int length = 0;
+
+  private:
+    double mean = 0;
+    double variance = 0;
+    double deviation = 0;
 };
