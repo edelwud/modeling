6b6fab2a6165a096ac8bebaf50141df8d7514848
edelwud <edelwud@icloud.com>
2021-09-14 00:29:41 +0300

    add: gauss distribution rendering capability

--- distribution/distribution/distribution-gui/include/distribution-gui/gauss_histogram.h
+++ distribution/distribution/distribution-gui/include/distribution-gui/gauss_histogram.h
@@ -1,6 +1,27 @@
 #pragma once
 
+#include <vector>
+
 class GaussDistributionHistogram {
   protected:
     void RenderGaussHistogram();
+
+  private:
+    void calculate();
+
+  private:
+    std::vector<int> positions;
+    std::vector<int> values;
+    std::vector<double> sequence;
+
+  private:
+    double seqMean = 0;
+    double std = 0;
+    int length = 0;
+    int n = 0;
+
+  private:
+    double mean = 0;
+    double variance = 0;
+    double deviation = 0;
 };
--- distribution/distribution/distribution-gui/src/gauss_histogram.cpp
+++ distribution/distribution/distribution-gui/src/gauss_histogram.cpp
@@ -1,11 +1,42 @@
 #include <gauss_histogram.h>
+#include <utils.h>
 
 #include <Mahi/Gui.hpp>
 #include <Mahi/Util.hpp>
 
+#include <distribution/distribution.h>
+#include <distribution/utils.h>
+
 void GaussDistributionHistogram::RenderGaussHistogram() {
     if (ImGui::BeginTabItem("Gauss")) {
-        ImGui::Text("Gauss distribution");
+        ImGui::InputDouble("mean", &seqMean);
+        ImGui::InputDouble("std", &std);
+        ImGui::InputInt("n", &n);
+        ImGui::InputInt("length", &length);
+        if (ImGui::Button("Apply"))
+            calculate();
+
+        ImGui::Text("Mean: %lf\nVariance: %lf\nDeviation: %lf", mean, variance,
+                    deviation);
+
+        ImPlot::FitNextPlotAxes(true, true, true, true);
+        if (ImPlot::BeginPlot("##GaussHistogram", nullptr, nullptr,
+                              ImVec2(-1, -1), ImPlotFlags_NoChild)) {
+            ImPlot::PlotBars("Gauss Levels", positions.data(),
+                             values.data(), positions.size(), 0.5f);
+            ImPlot::EndPlot();
+        }
+
         ImGui::EndTabItem();
     }
+}
+
+void GaussDistributionHistogram::calculate() {
+    sequence = Distribution::GenerateGaussDistribution(seqMean, std, n, length);
+    mean = Utils::CalculateSequenceMean(sequence);
+    variance = Utils::CalculateSequenceVariance(sequence, mean);
+    deviation = Utils::CalculateSequenceDeviation(sequence, mean);
+
+    positions = HistogramUtils::GenerateXPositions(20, 0, 1);
+    values = HistogramUtils::GetXRandomValuesCount(sequence, 20, 0, 1);
 }
