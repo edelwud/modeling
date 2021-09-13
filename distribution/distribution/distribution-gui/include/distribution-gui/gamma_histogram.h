1da54654a015e274180a527f1d316a4873aec491
edelwud <edelwud@icloud.com>
2021-09-13 19:13:22 +0300

    add: gamma histogram render capability

--- distribution/distribution/distribution-gui/include/distribution-gui/gamma_histogram.h
+++ distribution/distribution/distribution-gui/include/distribution-gui/gamma_histogram.h
@@ -1,6 +1,26 @@
 #pragma once
 
+#include <vector>
+
 class GammaDistributionHistogram {
   protected:
     void RenderGammaHistogram();
+
+  private:
+    void calculate();
+
+  private:
+    std::vector<int> positions;
+    std::vector<int> values;
+    std::vector<double> sequence;
+
+  private:
+    int eta = 0;
+    int length = 0;
+    double param = 0;
+
+  private:
+    double mean = 0;
+    double variance = 0;
+    double deviation = 0;
 };
--- distribution/distribution/distribution-gui/src/gamma_histogram.cpp
+++ distribution/distribution/distribution-gui/src/gamma_histogram.cpp
@@ -1,11 +1,41 @@
 #include <gamma_histogram.h>
+#include <utils.h>
 
 #include <Mahi/Gui.hpp>
 #include <Mahi/Util.hpp>
 
+#include <distribution/distribution.h>
+#include <distribution/utils.h>
+
 void GammaDistributionHistogram::RenderGammaHistogram() {
     if (ImGui::BeginTabItem("Gamma")) {
-        ImGui::Text("Gamma distribution");
+        ImGui::InputInt("eta", &eta);
+        ImGui::InputDouble("param", &param);
+        ImGui::InputInt("length", &length);
+        if (ImGui::Button("Apply"))
+            calculate();
+
+        ImGui::Text("Mean: %lf\nVariance: %lf\nDeviation: %lf", mean, variance,
+                    deviation);
+
+        ImPlot::FitNextPlotAxes(true, true, true, true);
+        if (ImPlot::BeginPlot("##GammaHistogram", nullptr, nullptr,
+                              ImVec2(-1, -1), ImPlotFlags_NoChild)) {
+            ImPlot::PlotBars("Gamma Levels", positions.data(),
+                             values.data(), positions.size(), 0.5f);
+            ImPlot::EndPlot();
+        }
+
         ImGui::EndTabItem();
     }
+}
+
+void GammaDistributionHistogram::calculate() {
+    sequence = Distribution::GenerateExponentialDistribution(param, length);
+    mean = Utils::CalculateSequenceMean(sequence);
+    variance = Utils::CalculateSequenceVariance(sequence, mean);
+    deviation = Utils::CalculateSequenceDeviation(sequence, mean);
+
+    positions = HistogramUtils::GenerateXPositions(20, 0, 1);
+    values = HistogramUtils::GetXRandomValuesCount(sequence, 20, 0, 1);
 }
