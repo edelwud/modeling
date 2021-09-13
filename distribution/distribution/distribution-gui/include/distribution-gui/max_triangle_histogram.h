9d5933b11a3d2e25e654377d9f40b31f7c1fdf28
edelwud <edelwud@icloud.com>
2021-09-14 00:34:40 +0300

    add: max triangle distribution rendering capability

--- distribution/distribution/distribution-gui/include/distribution-gui/max_triangle_histogram.h
+++ distribution/distribution/distribution-gui/include/distribution-gui/max_triangle_histogram.h
@@ -1,6 +1,26 @@
 #pragma once
 
+#include <vector>
+
 class MaxTriangleDistributionHistogram {
   protected:
     void RenderMaxTriangleHistogram();
+
+  private:
+    void calculate();
+
+  private:
+    std::vector<int> positions;
+    std::vector<int> values;
+    std::vector<double> sequence;
+
+  private:
+    int a = 0;
+    int b = 0;
+    int length = 0;
+
+  private:
+    double mean = 0;
+    double variance = 0;
+    double deviation = 0;
 };
--- distribution/distribution/distribution-gui/src/max_triangle_histogram.cpp
+++ distribution/distribution/distribution-gui/src/max_triangle_histogram.cpp
@@ -1,11 +1,41 @@
 #include <max_triangle_histogram.h>
+#include <utils.h>
 
 #include <Mahi/Gui.hpp>
 #include <Mahi/Util.hpp>
 
+#include <distribution/distribution.h>
+#include <distribution/utils.h>
+
 void MaxTriangleDistributionHistogram::RenderMaxTriangleHistogram() {
     if (ImGui::BeginTabItem("MaxTriangle")) {
-        ImGui::Text("MaxTriangle distribution");
+        ImGui::InputInt("a", &a);
+        ImGui::InputInt("b", &b);
+        ImGui::InputInt("length", &length);
+        if (ImGui::Button("Apply"))
+            calculate();
+
+        ImGui::Text("Mean: %lf\nVariance: %lf\nDeviation: %lf", mean, variance,
+                    deviation);
+
+        ImPlot::FitNextPlotAxes(true, true, true, true);
+        if (ImPlot::BeginPlot("##MaxTriangleHistogram", nullptr, nullptr,
+                              ImVec2(-1, -1), ImPlotFlags_NoChild)) {
+            ImPlot::PlotBars("MaxTriangle Levels", positions.data(),
+                             values.data(), positions.size(), 0.5f);
+            ImPlot::EndPlot();
+        }
+
         ImGui::EndTabItem();
     }
+}
+
+void MaxTriangleDistributionHistogram::calculate() {
+    sequence = Distribution::GenerateMaxTriangleDistribution(a, b, length);
+    mean = Utils::CalculateSequenceMean(sequence);
+    variance = Utils::CalculateSequenceVariance(sequence, mean);
+    deviation = Utils::CalculateSequenceDeviation(sequence, mean);
+
+    positions = HistogramUtils::GenerateXPositions(20, 0, 1);
+    values = HistogramUtils::GetXRandomValuesCount(sequence, 20, 0, 1);
 }
