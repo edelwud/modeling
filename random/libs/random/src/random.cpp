bf324e6f2f028777d351b90e1226be4c721c196f
edelwud <edelwud@icloud.com>
2021-09-13 11:45:04 +0300

    fix: static dependency

--- random/libs/random/src/random.cpp
+++ random/libs/random/src/random.cpp
@@ -2,8 +2,8 @@
 
 std::vector<double> Random::GenerateSequence(int a, int m, int r0, int length) {
     std::vector<double> sequence(length);
-    std::generate(sequence.begin(), sequence.end(), [a, m, r0]() {
-        static long long rn = r0;
+    long long rn = r0;
+    std::generate(sequence.begin(), sequence.end(), [a, m, &rn]() {
         rn = rn * a % m;
         return (double)rn / (double)m;
     });
