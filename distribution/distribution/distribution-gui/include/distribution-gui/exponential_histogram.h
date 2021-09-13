3d4027cc5ad347882c5f6288e87d398a821517e5
edelwud <edelwud@icloud.com>
2021-09-13 19:13:12 +0300

    add: exponential histogram render capability

--- distribution/distribution/distribution-gui/include/distribution-gui/exponential_histogram.h
+++ distribution/distribution/distribution-gui/include/distribution-gui/exponential_histogram.h
@@ -1,6 +1,25 @@
 #pragma once
 
+#include <vector>
+
 class ExponentialDistributionHistogram {
   protected:
     void RenderExponentialHistogram();
+
+  private:
+    void calculate();
+
+  private:
+    std::vector<int> positions;
+    std::vector<int> values;
+    std::vector<double> sequence;
+
+  private:
+    int length = 0;
+    double param = 0;
+
+  private:
+    double mean = 0;
+    double variance = 0;
+    double deviation = 0;
 };
